32445763c27efde6b7b24fcc6e04ebe5de67fa35
SHO-SAN-tokyo <info.shosanmk@gmail.com>
2025-04-18 13:27:54 +0900

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ st.title("🏠 HOME - ダッシュボード入口")
 st.markdown("### 🔗 ページ一覧")
 
 st.page_link("pages/01_🫧Ad_Drive.py", label="🫧 Ad Drive")
-st.page_link("pages/02_🧩SHO-SAN_market.py", label="🧩 SHO-SAN_market")
+st.page_link("pages/02_🧩SHO-SAN_market.py", label="🧩 SHO-SAN market")
 st.page_link("pages/07_⚙️Unit_Settings.py", label="⚙️ Unit設定")
 
 
