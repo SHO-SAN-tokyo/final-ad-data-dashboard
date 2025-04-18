bb52db71f433e25443244735543d02706d6bac66
SHO-SAN-tokyo <info.shosanmk@gmail.com>
2025-04-18 12:01:51 +0900

    Update and rename 1_🏠_ホーム.py to 1_🏠_Home_ホーム.py

--- 1_🏠_Home_ホーム.py
+++ 1_🏠_Home_ホーム.py
@@ -5,5 +5,5 @@ st.title("🏠 HOME - ダッシュボード入口")
 
 st.markdown("### 🔗 ページ一覧")
 
-st.page_link("pages/2_🫧_メインダッシュボード.py", label="🫧 Ad Drive")
-st.page_link("pages/3_⚙️_ユニット設定.py", label="⚙️ Unit設定")
+st.page_link("pages/2_📊_Main_ダッシュボード.py", label="🫧 Ad Drive")
+st.page_link("pages/3_⚙️_Unit_ユニット設定.py", label="⚙️ Unit設定")
