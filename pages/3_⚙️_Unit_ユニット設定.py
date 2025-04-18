ed9c077b2cb72d8ca1c125d15d201ecc22335912
SHO-SAN-tokyo <info.shosanmk@gmail.com>
2025-04-18 12:06:03 +0900

    Update and rename 3_⚙️_ユニット設定.py to 3_⚙️_Unit_ユニット設定.py

--- pages/3_⚙️_Unit_ユニット設定.py
+++ pages/3_⚙️_Unit_ユニット設定.py
@@ -2,7 +2,7 @@ import streamlit as st
 from google.cloud import bigquery
 import pandas as pd
 
-def render():
+st.set_page_config(page_title="Unit設定", layout="wide")
 st.title("⚙️ Unit設定ページ")
 
 # BigQuery 認証
