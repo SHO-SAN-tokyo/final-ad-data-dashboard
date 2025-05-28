fdef8517565fb2a76c2a0a8b86292cade1b0b3d8
SHO-SAN-tokyo <info.shosanmk@gmail.com>
2025-05-28 14:18:38 +0900

    Update 04_🔷Unit_Drive.py

--- pages/04_🔷Unit_Drive.py
+++ pages/04_🔷Unit_Drive.py
@@ -96,7 +96,7 @@ unit_colors = ["#c0e4eb", "#cbebb5", "#ffdda6"]
 unit_color_map = {unit: unit_colors[i % len(unit_colors)] for i, unit in enumerate(unit_summary["所属"].unique())}
 
 # --- Unitカード ---
-st.write("#### 🍋🍋‍🟩 Unitごとのスコア")
+st.write("#### 🍋🍋‍🟩 Unitごとのスコア 🍒🍏")
 unit_cols = st.columns(3)
 for idx, row in unit_summary.iterrows():
     with unit_cols[idx % 3]:
