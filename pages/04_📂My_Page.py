5f2ebbea20f2479505ded5fc0c82c92b26076abe
SHO-SAN-tokyo <info.shosanmk@gmail.com>
2025-06-26 23:53:42 +0900

    Update 04_📂My_Page.py

--- pages/04_📂My_Page.py
+++ pages/04_📂My_Page.py
@@ -1,1 +1,12 @@
+import streamlit as st
+import pandas as pd
+from google.cloud import bigquery
+import html
+
+# ──────────────────────────────────────────────
+# ログイン認証
+# ──────────────────────────────────────────────
+from auth import require_login
+require_login()
+
 st.markdown("###### じゅんびちゅうです。")
