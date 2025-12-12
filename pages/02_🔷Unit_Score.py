2d9d64b0c5aef04ed52d24944d60f6e3c3013c99
SHO-SAN-tokyo <info.shosanmk@gmail.com>
2025-12-12 10:47:29 +0900

    Update 02_🔷Unit_Score.py

--- pages/02_🔷Unit_Score.py
+++ pages/02_🔷Unit_Score.py
@@ -247,7 +247,7 @@ subcat_options = sorted(latest["サブカテゴリ"].dropna().astype(str).unique
 
 # ★ 初期フィルター
 default_employment = ["インターン"] if "インターン" in employment_options else []
-default_maincat = [x for x in maincat_options if x not in ["分譲住宅･土地", "分譲マンション"]]
+default_maincat = [x for x in maincat_options if x not in ["分譲マンション"]]
 default_subcat = [x for x in subcat_options if x not in ["認知", "採用", "分譲", "ページ流入"]]
 
 # UI（上段：注力度＋メインカテゴリ、下段：サブカテゴリ）
